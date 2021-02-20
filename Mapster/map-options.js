80fac1de665dcb6ed493db7253b4767e66f5d140
Max Smiley <maxsmiley0@gmail.com>
2021-02-20 03:45:15 -0800

    Added map options, and created a seperate file to store these options for modularity

---
+++ Mapster/map-options.js
@@ -0,0 +1,28 @@
+(function(window, google, mapster) {
+    mapster.MAP_OPTIONS = {
+        center: {
+            lat: 37.791350,
+            lng: -122.435883,
+        },
+        zoom: 10,
+        disableDefaultUI: true,
+        //Here, we set some random options for convenience. We can always revert these changes                                                                                       
+        //These values are taken from the google maps API page, I didn't define them myself                                                                                          
+        scrollwheel: false,
+        draggable: true,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        maxZoom: 20,
+        minZoom: 9,
+        zoomControl: true,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.LEFT_BOTTOM,
+            style: google.maps.ZoomControlStyle.DEFAULT,
+        },/*                                                                                                                                                                         
+        Haven't quite gotten this to work, but I'll keep it here in case I have a Eureka! moment...                                                                                  
+        panControl: true,                                                                                                                                                            
+        panControlOptions: {                                                                                                                                                         
+            position: google.maps.ControlPosition.LEFT_TOP,                                                                                                                          
+        },*/
+    };
+
+}(window, google, window.Mapster || (window.Mapster = {})))
--- Mapster/script.js
+++ Mapster/script.js
@@ -1,15 +1,9 @@
 //"Self-invoking anonymous function" - everything is contained within this scope
 //Passing in the window and google namespaces into this function scope
-(function(window, google) {
+(function(window, google, mapster) {
     //Object Literal, gives us some initial parameters
-    var options = {
-	center: {
-	    lat: 37.791350,
-	    lng: -122.435883,
-	},
-	zoom: 10
-    },
+    var options = mapster.MAP_OPTIONS
     //Creates our map
     element = document.getElementById('map-canvas'),
     map = new google.maps.Map(element, options);	
-}(window, google))
+}(window, google, window.Mapster || (window.Mapster = {})));

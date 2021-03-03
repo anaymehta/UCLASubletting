57edee0e98dbb2411e7157b0c3dafcb85f1cff0c
Anay Mehta <anaymehta10@gmail.com>
2021-03-02 22:07:38 -0800

    Update index.js
    Kaushik added get request code, routes.
    Also added author parameter to post request

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,14 @@ const saltRounds = 10;
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 
+const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+
+// get config vars
+dotenv.config();
+
+// access config var
+console.log(process.env.TOKEN_SECRET);
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,23 +33,30 @@ const uri = "mongodb+srv://dbUser:o0UhevJddzhSfEEa@cluster0.lpucd.mongodb.net/my
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 var db;
+
 client.connect(err => {
   db = client.db("sublettingDB")
   // perform actions on the collection object
 });
 
+function generateToken(identifier) {
+  //Currently using email as unique identifier, perhaps switch to ObjId for more convinient indexing later
+  return jwt.sign(identifier, process.env.TOKEN_SECRET); //No-expiry for testing purposes
+}
+
 app.post('/signUp', function (req, res) {
   //Missing validation of user not existing
   var users = db.collection('users');
   var plainPass = req.body.password; //Hash on client side in the future for better security
+  var accessToken = generateToken(req.body.email)
   console.log(plainPass);
   createUserDoc().then(() => {
     hashPass().then((genHash) => {
       console.log("Email: " + req.body.email);
       console.log("genhash: " + genHash);
-      users.updateOne({ "email": req.body.email }, { $set : { "password": genHash }})
+      users.updateOne({ "email": req.body.email }, { $set : { "password": genHash , "tok": accessToken}})
     }).then(() => {
-      res.send("User Created");
+      res.send(accessToken);
     })
   })
 
@@ -61,6 +76,39 @@ app.post('/signUp', function (req, res) {
 });
 
 
+app.post('/signIn', function (req, res) {
+   var users = db.collection('users');
+   var accessToken;
+   comparePass().then((result) => {
+     if(result === false) {
+       //password is didnt match / is wrong
+       console.log("Wrong password");
+       res.send("Wrong password")
+     } else {
+       console.log("correct password");
+       res.send(accessToken)
+     }
+   })
+
+   async function comparePass() {
+     var userData = await users.find({"email": req.body.email}).toArray();
+     return new Promise((resolve, reject) => {
+       accessToken = userData[0].tok
+       bcrypt.compare(req.body.password, userData[0].password, function(err, result) {
+         if(err) {
+           console.log(err);
+         } else {
+           console.log("bcrypt result: " + result);
+           resolve(result);
+         }
+       });
+     })
+   }
+
+
+})
+
+
 app.post('/createListing', function (req, res) {
   //Need to associate listingDoc ObjID with User
   var listings = db.collection('listings');
@@ -70,8 +118,29 @@ app.post('/createListing', function (req, res) {
     })
 
   async function createListingDoc() {
-    let listingDoc = await listings.insert({"token": req.body.token, "text": req.body.text, "beds": req.body.beds, "baths": req.body.baths, "sqft": req.body.sqft, "email": req.body.email, "phone": req.body.phone});
+    let listingDoc = await listings.insert({"token": req.body.token, "author": req.body.author, "text": req.body.text, "beds": req.body.beds, "baths": req.body.baths, "sqft": req.body.sqft, "email": req.body.email, "phone": req.body.phone});
     return 0
+  }
+
+});
+
+app.get('/getListings', function (req, res) {
+  //Missing validation of user not existing
+  var listings = db.collection('listings');
+  returnAllListings()
+
+  async function returnAllListings() {
+    /*let listingDocs = await listings.find({});
+    return listingDocs */
+    listings.find({}).toArray(function (err, result) {
+        if (err) {
+            console.log(err);
+            res.send("Error finding all listings. Check Server logs.")
+        } else {
+            console.log("Returning : " + result);
+            res.send(result);
+        }
+    })
   }
 
 });

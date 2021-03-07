2129765274b554598a56d1138deea863d4385fae
Kashi de Leede <kashideleede@gmail.com>
2021-03-06 22:54:22 -0800

    Sign up/sign is modal in navbar
    * Navbar now a class instead of a function
    * Sign up / sign in can be performed from navbar
    * Got rid of redundant pages (sign up sign in)
    * Forgot to import semantics in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
+import 'semantic-ui-css/semantic.min.css'
 import React, { useState } from "react";
 import Navbar from "./components/Navbar.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import addaproperty from "./components/pages/addaproperty";
-import Signup from "./components/pages/Signup";
-import SignIn from "./components/pages/SignIn";
 import SearchResult from "./components/pages/SearchResult";
 
 function App() {
@@ -15,8 +14,6 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/add-a-property" exact component={addaproperty} />
-        <Route path="/sign-up" exact component={Signup} />
-        <Route path="/sign-in" exact component={SignIn} />
         <Route path="/search-result" exact component={SearchResult} />
       </Switch>
     </Router>
